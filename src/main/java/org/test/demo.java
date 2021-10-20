d7112e4acb5667ced34cd65216f6518302176f1c
Soundarya10696 <ssoundarya1096@gmail.com>
2021-10-20 11:23:05 +0530

    project structure

---
+++ src/main/java/org/test/demo.java
@@ -0,0 +1,84 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class demo {
+    public static WebDriver driver;
+    
+    public static void browserLaunch(String browserName) {
+	switch(browserName)	{
+	
+	case  "chrome" :
+		WebDriverManager.chromedriver().setup();
+		driver=new  ChromeDriver();
+		break;
+		
+	case  "firefox" :
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+		
+		
+	case   "edge"  :
+		WebDriverManager.edgedriver().setup();
+		driver=new EdgeDriver();
+		 
+		
+		default:
+			break;
+	}	
+			
+    }
+     
+    //launch url
+    public static void Launchurl(String url) {
+	driver.get(url);
+	}
+    
+     //geturl
+    public static void geturl(String url) {
+		driver.get(url);
+		
+	}
+          //get current title
+    
+    public static String getcurrentTitle() {
+    	
+    	String text=driver.getTitle();
+    	return text;
+    	
+    }
+    
+       //get current url
+    public static String geturl() {
+    	String url=driver.getCurrentUrl();
+    	
+    	return url;
+    }
+    
+        //mouse click
+    public static void clickbtn(WebElement  a) {
+		a.click();
+	}
+		
+    public static void sendvalues(WebElement  a,String value) {
+		a.sendKeys(value);
+	}
+    
+    
+
+    
+ 
+    }	
+	
+	
+
+
+	
+
+
